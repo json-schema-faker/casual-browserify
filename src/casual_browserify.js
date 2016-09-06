9be0103d2d7807f8ee7567a6a8775adfbd9b4524
Mark Riedesel <mark@klowner.com>
2016-09-06 10:42:41 -0500

    don't use path.sep since safe_require is called with forward-slashes, fixes Windows issue

--- src/casual_browserify.js
+++ src/casual_browserify.js
@@ -54,7 +54,7 @@ var locales = {
     }
 };
 var safe_require = function(filename) {
-	var parts = filename.split(require('path').sep).slice(-2),
+	var parts = filename.split('/').slice(-2),
 		locale = parts[0],
 		provider = parts[1];
 
--- src/safe_require_browserify.js
+++ src/safe_require_browserify.js
@@ -1,6 +1,6 @@
 
 var safe_require = function(filename) {
-	var parts = filename.split(require('path').sep).slice(-2),
+	var parts = filename.split('/').slice(-2),
 		locale = parts[0],
 		provider = parts[1];
 
